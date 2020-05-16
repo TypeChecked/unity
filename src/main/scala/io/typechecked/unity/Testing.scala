59a5a59ed3484ad076b2da906d1103b41565f540
jdrphillips <james@typechecked.io>
2020-05-16 10:23:27 +0100

    Update3: Incremented + algorithms

--- src/main/scala/io/typechecked/unity/Testing.scala
+++ src/main/scala/io/typechecked/unity/Testing.scala
@@ -5,12 +5,6 @@ import io.typechecked.unity.fundamentals._
 import shapeless.tag
 import shapeless.tag.@@
 
-// Tags
-
-trait Incremented
-
-// End tags
-
 object Canonicals {
 
   implicit val idImpl: Canonical.Aux[UserIdConcept, UserId] = null
@@ -40,83 +34,14 @@ object Functions {
 
 
 
-  implicit def AgeImplIncrement: Fn[Age, Age @@ Incremented] = Fn { age =>
-    tag[Incremented][Age](Age(age.value + 1))
-  }
-
-  implicit def AgeImplLt: Fn[Age, Fn[Int, Boolean]] = Fn { age =>
-    Fn { limit =>
-      age.value < limit
-    }
-  }
 
 
-
-  implicit def BuildList[T, Impl](
-    implicit t: T |--> Impl
-  ): Impl ==>: List[Impl] = Fn { t: Impl => List(t) }
-
-  implicit def PrependList[T, Impl](
-    implicit t: T |--> Impl
-  ): List[Impl] ==>: Impl ==>: List[Impl] = Fn { list: List[Impl] =>
-    Fn { t: Impl => t :: list }
-  }
-
-  implicit def UserAgeIncremented[User <: UserConcept, Age <: AgeConcept](
-    implicit user: Canonical.Aux[UserConcept, User],
-    age: Canonical.Aux[AgeConcept, Age],
-    toAge: Fn[User, Age],
-    incr: Fn[Age, Age @@ Incremented]
-  ): Fn[User, Age @@ Incremented] = Fn { user: User => incr(toAge(user)) }
 
 }
 
 object FunctionsAbstract {
 
-  implicit def UserAgePlusOneIsTooYoung[User <: UserConcept, Age <: AgeConcept](
-    implicit user: UserConcept |--> User,
-    age: Canonical.Aux[AgeConcept, Age],
-    userAgePlusOne: Fn[User, Age @@ Incremented],
-    ageLt: Age ==>: Int ==>: Boolean
-  ): Fn[User, Boolean] = Fn { user: User =>
-    val newAge = userAgePlusOne(user)
-    ageLt(newAge)(18)
-  }
 
-  implicit def NextFiveAgesForUser[User <: UserConcept, Age <: AgeConcept](
-    implicit user: UserConcept |--> User,
-    age: AgeConcept |--> Age,
-    getAge: User ==>: Age,
-    incr: Age ==>: (Age @@ Incremented),
-    list: Age ==>: List[Age],
-    prepend: List[Age] ==>: Age ==>: List[Age]
-  ): User ==>: List[Age] = Fn { user =>
-    val age1 = getAge(user)
-    val age2 = incr(age1)
-    val age3 = incr(age2)
-    val age4 = incr(age3)
-    val age5 = incr(age4)
-
-    val list1 = list(age1)
-    val list2 = prepend(list1)(age2)
-    val list3 = prepend(list2)(age3)
-    val list4 = prepend(list3)(age4)
-    val list5 = prepend(list4)(age5)
-
-    list5
-  }
-
-  implicit def AgeAndNamePair[User <: UserConcept, Age <: AgeConcept, Name <: NameConcept, Pair[_, _] <: PairConcept[_, _]](
-    implicit user: UserConcept |--> User,
-    age: AgeConcept |--> Age,
-    name: NameConcept |--> Name,
-    pair: PairConcept[AgeConcept, NameConcept] |--> Pair[Age, Name],
-    getAge: User ==>: Age,
-    getName: User ==>: Name,
-    buildPair: Age ==>: Name ==>: Pair[Age, Name]
-  ): User ==>: Pair[Age, Name] = Fn { user =>
-    buildPair(getAge(user))(getName(user))
-  }
 
 }
 
---
+++ src/main/scala/io/typechecked/unity/update3/Update.scala
@@ -0,0 +1,85 @@
+package io.typechecked.unity.update3
+
+import io.typechecked.unity.fundamentals._
+import io.typechecked.unity.update1.Concepts.Age
+
+import shapeless.tag
+import shapeless.tag.@@
+
+object Concepts {
+
+  // A type tag
+  trait Incremented
+
+}
+
+object Functions {
+
+  import Concepts._
+
+  // Increment an age
+  implicit def AgeImplIncrement(
+    implicit constructor: Int ==>: Age
+  ): Age ==>: (Age @@ Incremented) = Fn { age =>
+    tag[Incremented][Age](constructor(age.value + 1))
+  }
+
+  // Algorithm to return a user's incremented age
+  implicit def UserAgeIncremented[User <: UserConcept, Age <: AgeConcept](
+    implicit user: UserConcept |--> User,
+    age: AgeConcept |--> Age,
+    toAge: User ==>: Age,
+    incr: Age ==>: (Age @@ Incremented)
+  ): User ==>: (Age @@ Incremented) = Fn { user: User => incr(toAge(user)) }
+
+  // Lift to List
+  implicit def BuildList[T, Impl](
+    implicit t: T |--> Impl
+  ): Impl ==>: List[Impl] = Fn { t: Impl => List(t) }
+
+  // List prepend
+  implicit def PrependList[T, Impl](
+    implicit t: T |--> Impl
+  ): List[Impl] ==>: Impl ==>: List[Impl] = Fn { list: List[Impl] =>
+    Fn { t: Impl => t :: list }
+  }
+
+  // Algorithm to return next five ages a user will be
+  implicit def NextFiveAgesForUser[User <: UserConcept, Age <: AgeConcept](
+    implicit user: UserConcept |--> User,
+    age: AgeConcept |--> Age,
+    getAge: User ==>: Age,
+    incr: Age ==>: (Age @@ Incremented),
+    list: Age ==>: List[Age],
+    prepend: List[Age] ==>: Age ==>: List[Age]
+  ): User ==>: List[Age] = Fn { user =>
+    val age1 = getAge(user)
+    val age2 = incr(age1)
+    val age3 = incr(age2)
+    val age4 = incr(age3)
+    val age5 = incr(age4)
+
+    val list1 = list(age1)
+    val list2 = prepend(list1)(age2)
+    val list3 = prepend(list2)(age3)
+    val list4 = prepend(list3)(age4)
+    val list5 = prepend(list4)(age5)
+
+    list5
+  }
+
+  // Algorithm to return the age and name from a user in a pair
+  implicit def AgeAndNamePair[User <: UserConcept, Age <: AgeConcept, Name <: NameConcept, Pair[_, _] <: PairConcept[_, _]](
+    implicit user: UserConcept |--> User,
+    age: AgeConcept |--> Age,
+    name: NameConcept |--> Name,
+    pair: PairConcept[AgeConcept, NameConcept] |--> Pair[Age, Name],
+    getAge: User ==>: Age,
+    getName: User ==>: Name,
+    buildPair: Age ==>: Name ==>: Pair[Age, Name]
+  ): User ==>: Pair[Age, Name] = Fn { user =>
+    buildPair(getAge(user))(getName(user))
+  }
+
+
+}
